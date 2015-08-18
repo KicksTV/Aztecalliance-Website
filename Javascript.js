5396046b6b15492746c6c4c657a73e75e554c940
KicksTV <Kicksmedia1@gmail.com>
2015-08-18 13:39:02 +0100

    added everything

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+// Code for logo animation
+$(document).ready(function() {
+	$("#aztec").hide(0).slideDown(1000);
+	});
+
+// Nav bar 
+	$('.handle').on('click', function(){
+	$('nav ul').toggleClass('showing');
+	});
